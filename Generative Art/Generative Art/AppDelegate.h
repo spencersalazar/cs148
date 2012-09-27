e916468825f60de5dd83d74ce2d886a0b0ed5c0b
Spencer Salazar <spencer.salazar@gmail.com>
2012-09-26 19:28:26 -0700

    initial commit

---
+++ Generative Art/Generative Art/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Generative Art
+//
+//  Created by Spencer Salazar on 9/26/12.
+//  Copyright (c) 2012 Spencer Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
---
+++ Generative Art/Generative Art/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Generative Art
+//
+//  Created by Spencer Salazar on 9/26/12.
+//  Copyright (c) 2012 Spencer Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

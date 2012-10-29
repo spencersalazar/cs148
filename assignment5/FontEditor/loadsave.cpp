7415e6385f2286a96b359c9cce8fc798b86152f5
Spencer Salazar <spencer.salazar@gmail.com>
2012-10-28 20:40:24 -0700

    load/save etc.

---
+++ assignment5/FontEditor/loadsave.cpp
@@ -0,0 +1,9 @@
+//
+//  loadsave.cpp
+//  FontEditor
+//
+//  Created by Spencer Salazar on 10/28/12.
+//
+//
+
+#include "loadsave.h"
---
+++ assignment5/FontEditor/loadsave.h
@@ -0,0 +1,14 @@
+//
+//  loadsave.h
+//  FontEditor
+//
+//  Created by Spencer Salazar on 10/28/12.
+//
+//
+
+#ifndef __FontEditor__loadsave__
+#define __FontEditor__loadsave__
+
+#include <iostream>
+
+#endif /* defined(__FontEditor__loadsave__) */
